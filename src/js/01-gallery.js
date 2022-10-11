57b0057151dd03a3b73f0b871d330833e591901f
SvetlanaDaizy <103560817+SvetlanaDaizy@users.noreply.github.com>
2022-10-11 19:40:20 +0300

    22

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+
+const createListImg = galleryItems.map(({ preview, original, description }) => {
+    return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+})
+
+galleryDiv.innerHTML = createListImg.join('');
+
+
+galleryDiv.addEventListener('click', onGalleryClick);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+
+player.on('eventName', function(data) {
+    // data is an object containing properties specific to that event
+});
+
+var callback = function() {};
+
+player.off('eventName', callback);
